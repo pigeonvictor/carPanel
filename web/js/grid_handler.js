38728554e95d897fdfcc6424e5be42036cdd9da7
Victor <pigeonvictor@gmail.com>
2015-09-16 01:30:55 +0200

    colorpicker + changement de couleur au clik

--- web/js/grid_handler.js
+++ web/js/grid_handler.js
@@ -1,23 +1,52 @@
 var Square = React.createClass({
-    render :function (){
-      console.log(this.props.div_width);
-      var square_size = Math.floor((this.props.div_width - 1) / this.props.nb_squares);
-      if (square_size > 150)
-        square_size = 150;
-      console.log(square_size);
+  getInitialState : function(){
+    return {
+      backcolor : '#000000'
+    };
+  },
+  handleClick : function(){
+    console.log('COUCOU !');
+    this.setState({backcolor : document.getElementById('colorpicker').value});
+  },
+    render : function(){
+      console.log("A fucking Square of " + this.props.square_size);
       var style = {
-        width:square_size,
-        height:square_size
+        width:this.props.square_size,
+        height:this.props.square_size,
+        background:this.state.backcolor
       }
       return (
-      <div className="container">
-        {this.props.raw.map(function (i){
-          return <div className='square'
-                      style={style}></div>
-        })}
-      </div>
+        <div className='square' style={style} onClick={this.handleClick}></div>
       );
     }
+})
+
+var SquareRow = React.createClass({
+  getComponent: function(index) {
+    $(this.getDOMNode()).find('div:nth-child(' + index + ')').css({
+      'background-color': '#AE42F0'
+    });
+  },
+
+  render : function (){
+    console.log(this.props.div_width);
+    var square_size = Math.floor((this.props.div_width - 1) / this.props.nb_squares);
+    if (square_size > 150)
+    square_size = 150;
+    console.log(square_size);
+    var style = {
+      width:square_size,
+      height:square_size
+    }
+    var i = 0;
+    return (
+      <div className="container">
+      {this.props.raw.map(function(i){
+        return <Square square_size={square_size}/>;
+      })}
+      </div>
+    );
+  }
 });
 
 var SquareDrawer = React.createClass({
@@ -39,49 +68,49 @@ var SquareDrawer = React.createClass({
           var size = 0, key;
           for (key in i){
             if (i.hasOwnProperty(key))
-              size++;
+            size++;
           }
-          return <Square raw={i}
-                       div_width={document.getElementById("squares").offsetWidth}
-                       nb_squares={size}/>;
-      })}
-      </div>
-    );
-  }
-});
+          return <SquareRow raw={i}
+          div_width={document.getElementById("squares").offsetWidth}
+          nb_squares={size}/>;
+        })}
+        </div>
+      );
+    }
+  });
 
-var SizeSelector = React.createClass({
-  getInitialState: function(){
-     return {
-       x : 0,
-       y : 0,
-     }
-  },
-  render: function(){
-    return (
-      <div className='container'>
-      <div className='input-group col-md-2'>
-          <span className="input-group-addon" id="basic-addon1">whidth</span>
-          <input  type="number" className="form-control" placeholder="X"
-                aria-describedby="basic-addon1" onChange={this.changeX}></input>
-          </div>
-          <div className='input-group col-md-2'>
-          <span className="input-group-addon" id="basic-addon1">heigh</span>
-          <input  type="number" className="form-control" placeholder="Y"
-                aria-describedby="basic-addon1" onChange={this.changeY}></input>
-          </div>
-          <SquareDrawer x={this.state.x} y={this.state.y}/>
-          </div>
-    );
-  },
-  changeX : function(event){
-    this.setState({x: event.target.value})
-  },
-  changeY : function(event){
-    this.setState({y: event.target.value})
-  }
+  var SizeSelector = React.createClass({
+    getInitialState: function(){
+      return {
+        x : 0,
+        y : 0,
+      }
+    },
+    render: function(){
+      return (
+        <div className='container'>
+        <div className='input-group col-md-2'>
+        <span className="input-group-addon" id="basic-addon1">whidth</span>
+        <input  type="number" className="form-control" placeholder="X"
+        aria-describedby="basic-addon1" onChange={this.changeX}></input>
+        </div>
+        <div className='input-group col-md-2'>
+        <span className="input-group-addon" id="basic-addon1">heigh</span>
+        <input  type="number" className="form-control" placeholder="Y"
+        aria-describedby="basic-addon1" onChange={this.changeY}></input>
+        </div>
+        <SquareDrawer x={this.state.x} y={this.state.y}/>
+        </div>
+      );
+    },
+    changeX : function(event){
+      this.setState({x: event.target.value})
+    },
+    changeY : function(event){
+      this.setState({y: event.target.value})
+    }
 
-});
+  });
 
 
-React.render(<SizeSelector/>, document.getElementById('size_selector'));
+  React.render(<SizeSelector/>, document.getElementById('size_selector'));
